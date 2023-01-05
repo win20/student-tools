99e34285d2465c529cfdcbfe0deb881383eba00a
Win Barua <win.barua2@gmail.com>
2023-01-05 23:38:38 +0000

    Setup backend and coding standards

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
-import NavBar from "../../shared/NavBar";
-import SearchBar from "./SearchBar";
-import FilterPill from "./FilterPill";
+import NavBar from '../../shared/NavBar';
+import SearchBar from './SearchBar';
+import FilterPill from './FilterPill';
 
 const Home = () => {
-  return (
-    <div id="Home">
-      <NavBar />
-      <h2 className="text-3xl pt-10 pl-5 font-semibold">
-        Calculators and Formulas
-      </h2>
-      <SearchBar />
-      <FilterPill />
-    </div>
-  );
+	return (
+		<div id="Home">
+			<NavBar />
+			<h2 className="text-3xl pt-10 pl-5 font-semibold">
+				Calculators and Formulas
+			</h2>
+			<SearchBar />
+			<FilterPill />
+		</div>
+	);
 };
 
 export default Home;
---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from 'express';
+const app: Express = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello world!!');
+});
+
+// start the Express server
+app.listen(port, () => {
+	// tslint:disable-next-line:no-console
+	console.log(`server started at http://localhost:${port}`);
+});
