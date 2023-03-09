004369739ae9e68fafa047a5a5aaa77b974f47b4
Win Barua <win.barua2@gmail.com>
2023-03-09 21:51:51 +0000

    Added suggested tools to fraction to percentage page

--- client/src/pages/Tools/FractionToPercentage/FractionToPercentageIndex.tsx
+++ client/src/pages/Tools/FractionToPercentage/FractionToPercentageIndex.tsx
@@ -1,6 +1,15 @@
 import RenderBigScreen from './FractionToPercentageRender';
+import SuggestedTools from 'shared/SuggestedTools';
+import { Helpers } from 'utils/helpers';
+import { useParams } from 'react-router-dom';
+import { Tool } from 'utils/tools';
 
 const FractionToPercentageIndex = () => {
+	const { toolid } = useParams();
+
+	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+	const toolsToSuggest: Tool[] = Helpers.getToolsToSuggest(toolid!);
+
 	return (
 		<div className="md:mt-10 responsive-spacing">
 			<h1 className="text-xl font-semibold md:text-3xl">
@@ -8,6 +17,8 @@ const FractionToPercentageIndex = () => {
 			</h1>
 
 			<RenderBigScreen />
+
+			<SuggestedTools toolsArray={toolsToSuggest} />
 		</div>
 	);
 };
