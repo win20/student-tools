7954e0beb19e49f248d454c947a71e3759fe92b5
Win Barua <win.barua2@gmail.com>
2023-03-18 16:22:59 +0000

    University finder frontend

--- client/src/pages/UniversityFinder/UniversityFinderIndex.tsx
+++ client/src/pages/UniversityFinder/UniversityFinderIndex.tsx
@@ -3,7 +3,27 @@ import Header from 'shared/Header';
 const UniversityFinderIndex = () => {
 	return (
 		<>
-			<h1>University Finder</h1>
+			<Header />
+			<div className="md:mt-10 responsive-spacing">
+				<h1 className="text-xl font-semibold md:text-3xl">University Finder</h1>
+
+				<form action="post" className="mt-4">
+					<label htmlFor="ucas-points" className="text-sm">
+						Predicted UCAS tariff points
+						<input
+							name="ucas-points"
+							type="text"
+							className="border-1 rounded-md py-1 px-2"
+						/>
+					</label>
+					<button
+						className="bg-secondary text-white font-semibold py-1 px-4 rounded-full mt-1 ml-4"
+						type="submit"
+					>
+						Search
+					</button>
+				</form>
+			</div>
 		</>
 	);
 };
